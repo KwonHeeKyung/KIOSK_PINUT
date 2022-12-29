9d2c4e1f07085b1ec142db92a622e4f62fc5807c
ksana1215 <ksana1215@naver.com>
2022-12-29 13:19:50 +0900

    Update request_main.py

--- src/request_main.py
+++ src/request_main.py
@@ -88,7 +88,7 @@ def admin_close():
     else:
         rd.set('msg', '001')
 
-# 이벤트 알림
+# 장치 알림
 def device_err():
     log_time = datetime.datetime.now()
     log_time = log_time.strftime("%Y-%m-%d-%H:%M:%S")
@@ -98,20 +98,52 @@ def device_err():
     elif err_type == b'lock':
         text_type = '문 여닫힘 에러'
         event_code = 'IM_KIOSK_01'
+        res = requests.post(f'{cf_network_server}kakao_alarm',
+                            json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                  "alarmHeader": "alarm", 'subjectHeader': "키오스크", 'alarmContext': text_type},
+                            verify=False)
+        res_2 = requests.post(f'{cf_network_server}kiosk_status',
+                              json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                    "event_code": event_code}, verify=False)
+        logger.info(f'[{log_time} | DEVICE ERROR]')
+        logger.info(res.text.replace('\n', '') + '\n' + res_2.text.replace('\n', ''))
     elif err_type == b'except':
         rd.set('msg', 'device_err')
         text_type = 'USB 장치 에러'
         event_code = 'IM_KIOSK_02'
+        res = requests.post(f'{cf_network_server}kakao_alarm',
+                            json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                  "alarmHeader": "alarm", 'subjectHeader': "키오스크", 'alarmContext': text_type},
+                            verify=False)
+        res_2 = requests.post(f'{cf_network_server}kiosk_status',
+                              json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                    "event_code": event_code}, verify=False)
+        logger.info(f'[{log_time} | DEVICE ERROR]')
+        logger.info(res.text.replace('\n', '') + '\n' + res_2.text.replace('\n', ''))
     elif err_type == b'long':
         text_type = '장시간 문열림'
         event_code = 'IM_KIOSK_03'
+        res = requests.post(f'{cf_network_server}kakao_alarm',
+                            json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                  "alarmHeader": "alarm", 'subjectHeader': "키오스크", 'alarmContext': text_type},
+                            verify=False)
+        res_2 = requests.post(f'{cf_network_server}kiosk_status',
+                              json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                    "event_code": event_code}, verify=False)
+        logger.info(f'[{log_time} | DEVICE ERROR]')
+        logger.info(res.text.replace('\n', '') + '\n' + res_2.text.replace('\n', ''))
     elif err_type == b'payment':
         text_type = '결제 에러'
         event_code = 'IM_KIOSK_04'
-    res = requests.post(f'{cf_network_server}kakao_alarm', json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id, "alarmHeader": "alarm", 'subjectHeader': "키오스크", 'alarmContext': text_type}, verify=False)
-    res_2 = requests.post(f'{cf_network_server}kiosk_status', json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id, "event_code": event_code}, verify=False)
-    logger.info(f'[{log_time} | DEVICE ERROR]')
-    logger.info(res.text.replace('\n', '') + '\n' + res_2.text.replace('\n', ''))
+        res = requests.post(f'{cf_network_server}kakao_alarm',
+                            json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                  "alarmHeader": "alarm", 'subjectHeader': "키오스크", 'alarmContext': text_type},
+                            verify=False)
+        res_2 = requests.post(f'{cf_network_server}kiosk_status',
+                              json={'companyId': cf_company_id, 'storeId': cf_store_id, 'deviceId': cf_device_id,
+                                    "event_code": event_code}, verify=False)
+        logger.info(f'[{log_time} | DEVICE ERROR]')
+        logger.info(res.text.replace('\n', '') + '\n' + res_2.text.replace('\n', ''))
 
 #이벤트 해제
 def release_event():

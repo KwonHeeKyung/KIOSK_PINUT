8d1ab9e061feff33a6bb1b496c5b5e1f4f4d8980
ksana1215 <ksana1215@naver.com>
2022-12-29 09:11:45 +0900

    Update door_main.py
    방어로직 재설계. 자동 재실행 로직 추가

--- src/door_main.py
+++ src/door_main.py
@@ -1,7 +1,6 @@
 # Made by Kim.Seung.Hwan / ksana1215@interminds.ai
 # -*- coding: utf-8 -*-
-import threading
-
+import os
 import serial
 import redis
 import logging
@@ -10,78 +9,81 @@ import config
 import urllib3
 from playsound import playsound
 import request_main
+
 cf_path = config.path['path']
 cf_door_port = config.refrigerators['door']
 rd = redis.StrictRedis(host='localhost', port=6379, db=0)
 Arduino = serial.Serial(port=cf_door_port, baudrate=9600, timeout=0.1)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
-logging.basicConfig(filename=cf_path+'kiosk_status.log',level=logging.DEBUG)
+logging.basicConfig(filename=cf_path + 'kiosk_status.log', level=logging.DEBUG)
 logger = logging.getLogger('UNO_LOG')
 cnt = 0
-
-def alarm():
-    status = rd.get('door')
-    global cnt
-    if status == b'customer' or status == b'admin_open':
-        cnt += 1
-        if cnt > 600:
-            logger.info(log_time)
-            if status == b'customer':
-                playsound(cf_path + 'voice/' + "long.mp3", False)
-                rd.set('err_type', 'long')
-                request_main.device_err()
-                cnt = 0
-            elif status == b'admin_open':
-                playsound(cf_path + 'voice/' + "longlong.mp3", False)
-                cnt = 0
-
-        threading.Timer(1, alarm).start()
-    else:
-        cnt = 0
-        return
-
+flg = 0
 while True:
     log_time = datetime.datetime.now()
     log_time = log_time.strftime("%Y-%m-%d-%H:%M:%S")
     door = rd.get('door')
     try:
         uno = str(Arduino.readline().decode('utf-8').rstrip())
-        #문열림
+        # 문열림
         if door == b'open':
+            logger.info(f'[{log_time} | DOOR_OPEN --> CLIENT]')
             Arduino.write(str('1').encode('utf-8'))
             rd.set('door', 'customer')
-            logger.info(f'[{log_time} | DOOR_OPEN --> CLIENT]')
             request_main.door_open()
-            alarm()
-
-        #관리자 문열림
+        # 100초 알림
+        elif door == b'customer' or door == b'admin_open':
+            cnt += 1
+            if cnt > 3000:
+                logger.info(log_time)
+                if door == b'customer':
+                    playsound(cf_path + 'voice/' + "long.mp3", False)
+                    rd.set('err_type', 'long')
+                    request_main.device_err()
+                    cnt = 0
+                elif door == b'admin_open':
+                    playsound(cf_path + 'voice/' + "longlong.mp3", False)
+                    cnt = 0
+        else:
+            cnt = 0
+        # 관리자 문열림
         if door == b'admin':
             Arduino.write(str('1').encode('utf-8'))
             rd.set('door', 'admin_open')
             request_main.admin_open()
-            alarm()
-
-        #문닫힘
+        # 문닫힘
         if uno == '0':
-            #관리자 권한
+            # 관리자 권한
             if door == b'admin_open':
                 request_main.admin_close()
-            #고객
+            # 고객
             elif door == b'customer':
                 logger.info(f'[{log_time} | DOOR_CLOSE --> CLIENT]')
                 rd.delete('door')
-                # rd.set('msg', 'door_close')
+                rd.set("msg", 'infer')
                 request_main.door_close()
-        #문여닫힘 에러
+                if rd.get('err_type') == b'long':
+                    request_main.release_event()
+        # 문여닫힘 방어로직 작동
         if uno == '2':
-            rd.set('err_type','except')
+            flg += 1
+        # 방어로직 두번 돌았으면 에러로 판정
+        if uno == '2' and flg == 1:
+            rd.set('err_type', 'lock')
+            logger.info(f'[{log_time} | LOCK ERR]')
+            request_main.door_close()
             request_main.device_err()
-            logger.info(f'[{log_time} | DOOR LOCK ERR]')
+        # 문 다시 닫힘 / 관리자 오클 / 키오스크 재실행
+        if uno == 'r' and flg >= 1:
+            logger.info(f'[{log_time} | DOOR RECLOSED]')
+            request_main.admin_open()
+            request_main.admin_close()
+            logger.info(f'[{log_time} | REBOOT]')
+            os.system('start.sh')
     except Exception as err:
         rd.set('err_type', 'except')
         rd.set('msg', 'device_err')
-        rd.delete('door')
         request_main.device_err()
         logger.info(f'[{log_time} | ARDUINO FAIL]' + '\n' + str(err))
         break
